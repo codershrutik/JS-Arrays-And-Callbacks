6a84da74261beb4f4b5ec8aa059f05f00d224795
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-06 10:09:41 +0000

    Arrow functions added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+// const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
 
-const doubles = numbers.map(function(num){
-    return num*2;
-})
+// const doubles = numbers.map(function(num){
+//     return num*2;
+// })
 
 // numbers.forEach(function (el){
 //     if(el % 2 == 0){
@@ -14,25 +14,41 @@ const doubles = numbers.map(function(num){
 //     console.log(el);
 // }
 
-const movies = [
-    {
-        title: 'Parasite',
-        score: 99
-    },
-    {
-        title: 'Black Panther',
-        score: 99
-    },
-    {
-        title: Lagaan,
-        score: 99
-    },
-    {
-        title: 'RRR',
-        score: 99
-    }
-]
+// const movies = [
+//     {
+//         title: 'Parasite',
+//         score: 99
+//     },
+//     {
+//         title: 'Black Panther',
+//         score: 99
+//     },
+//     {
+//         title: Lagaan,
+//         score: 99
+//     },
+//     {
+//         title: 'RRR',
+//         score: 99
+//     }
+// ]
 
-const titles = movies.map(function(movies){
-    return movies.title;
-})
+// const titles = movies.map(function(movies){
+//     return movies.title;
+// })
+
+// const add = function(x,y){
+//     return x+y;
+// }
+
+const add = (x,y) =>{
+    return x+y;
+}
+
+const square = (x) =>{
+    return x*x;
+}
+
+const rollDie = () =>{
+    return Math.floor(Math.random()*6)+1;
+}
