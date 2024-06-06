0d3404c49abaf50ae80626ca9d53da0a52047616
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-06 10:23:55 +0000

    Added filter function

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-// const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+
+numbers.filter(n=>{
+    return n<10;
+})
 
 // const doubles = numbers.map(function(num){
 //     return num*2;
@@ -14,24 +18,26 @@
 //     console.log(el);
 // }
 
-// const movies = [
-//     {
-//         title: 'Parasite',
-//         score: 99
-//     },
-//     {
-//         title: 'Black Panther',
-//         score: 99
-//     },
-//     {
-//         title: Lagaan,
-//         score: 99
-//     },
-//     {
-//         title: 'RRR',
-//         score: 99
-//     }
-// ]
+const movies = [
+    {
+        title: 'Parasite',
+        score: 99
+    },
+    {
+        title: 'Black Panther',
+        score: 99
+    },
+    {
+        title: Lagaan,
+        score: 99
+    },
+    {
+        title: 'RRR',
+        score: 99
+    }
+]
+
+const goodMovies = movies.filter(score>=90);
 
 // const titles = movies.map(function(movies){
 //     return movies.title;
--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
-setTimeout(() =>{
-    console.log("...are you still there?")
-},3000)
-console.log('BYE!');
+// setTimeout(() =>{
+//     console.log("...are you still there?")
+// },3000)
+// console.log('BYE!');
 
-const id = setInterval(() =>{
-    console.log(Math.random())
-},2000)
+// const id = setInterval(() =>{
+//     console.log(Math.random())
+// },2000)
+
