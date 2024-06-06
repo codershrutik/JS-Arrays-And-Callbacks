d342df6b16ca65c9eb923ee2c14b78279a51bd44
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-06 10:04:04 +0000

    Maps added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
 
-numbers.forEach(function (el){
-    if(el % 2 == 0){
-        console.log(el)
-    }
+const doubles = numbers.map(function(num){
+    return num*2;
 })
 
-for(let el of numbers){
-    console.log(el);
-}
+// numbers.forEach(function (el){
+//     if(el % 2 == 0){
+//         console.log(el)
+//     }
+// })
+
+// for(let el of numbers){
+//     console.log(el);
+// }
+
+const movies = [
+    {
+        title: 'Parasite',
+        score: 99
+    },
+    {
+        title: 'Black Panther',
+        score: 99
+    },
+    {
+        title: Lagaan,
+        score: 99
+    },
+    {
+        title: 'RRR',
+        score: 99
+    }
+]
+
+const titles = movies.map(funcyion(movies){
+    return movies.title;
+})
