fff5c5bf59c5349cd191f8a0fefb47082b46ae2f
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-06 10:20:26 +0000

    More methods added

--- src/app.js
+++ src/app.js
@@ -53,8 +53,8 @@
 //     return Math.floor(Math.random()*6)+1;
 // }
 
-const rollDie = () =>(
-    Math.floor(Math.random()*6)+1
-)
+// const rollDie = () =>(
+//     Math.floor(Math.random()*6)+1
+// )
 
-const add = (a,b) =>(a+b)
+// const add = (a,b) =>(a+b)
---
+++ src/script.js
@@ -0,0 +1,8 @@
+setTimeout(() =>{
+    console.log("...are you still there?")
+},3000)
+console.log('BYE!');
+
+const id = setInterval(() =>{
+    console.log(Math.random())
+},2000)
