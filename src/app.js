b6cf582e0f812987b5fee5bf899589201fc8c520
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-06 10:14:17 +0000

    Implicit arrow functions added

--- src/app.js
+++ src/app.js
@@ -41,14 +41,20 @@
 //     return x+y;
 // }
 
-const add = (x,y) =>{
-    return x+y;
-}
+// const add = (x,y) =>{
+//     return x+y;
+// }
 
-const square = (x) =>{
-    return x*x;
-}
+// const square = (x) =>{
+//     return x*x;
+// }
 
-const rollDie = () =>{
-    return Math.floor(Math.random()*6)+1;
-}
+// const rollDie = () =>{
+//     return Math.floor(Math.random()*6)+1;
+// }
+
+const rollDie = () =>(
+    Math.floor(Math.random()*6)+1
+)
+
+const add = (a,b) =>(a+b)
