644dee7a2fa71d9e2fe2d1fd993c5aae086b3446
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-07 11:21:00 +0000

    This keyword added within functions

--- src/new.js
+++ src/new.js
@@ -1,11 +1,24 @@
-const prices = [9,10,11,12,60,74,35];
+// const prices = [9,10,11,12,60,74,35];
 
-let Total = 0;
-for(let price of prices){
-    Total += price
+// let Total = 0;
+// for(let price of prices){
+//     Total += price
+// }
+// console.log(total)
+
+// const Total = prices.reduce((Total, price)=>{
+//     return total+price
+// })
+
+const person = {
+    firstName: 'Viggo',
+    lastName: 'Mortenson',
+    fullName: function(){
+        return `${this.firstName} ${this.lastName}`
+    },
+    shoutName: function(){
+        setTimeout(()=>{
+            console.log(this),
+        console.timeLog(this.fullName())
+        },3000)
 }
-console.log(total)
-
-const Total = prices.reduce((Total, price)=>{
-    return total+price
-})
